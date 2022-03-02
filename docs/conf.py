54488ac302dbabaea0eea9f304b966ad9a1ff4d2
bclaremar <70746791+bclaremar@users.noreply.github.com>
2022-03-02 15:24:53 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_lesson"
+extensions = ["sphinx_lesson",
+              'sphinx-prompt'
 ]
 
 myst_enable_extensions = [
