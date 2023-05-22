573275ef7936368c32864f53124427f1fa21d928
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:35:46 +0200

    Update integration2d_mpi.sh

--- Exercises/examples/hpc2n/integration2d_mpi.sh
+++ Exercises/examples/hpc2n/integration2d_mpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A SNIC2022-22-641
+#SBATCH -A hpc2n2023-089
 #SBATCH -t 00:05:00
 #SBATCH -n 4
 #SBATCH -o output_%j.out   # output file
@@ -7,7 +7,7 @@
 
 # Set a path where the example programs are installed, provided you followed the suggestion. 
 # In any case, change the below to your own path to where you placed the example programs
-MYPATH=/proj/nobackup/snic2022-22-641/<mydir-name>/pythonHPC2N/examples/programs/
+MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/pythonHPC2N/examples/programs/
 
 ml purge > /dev/null 2>&1
 ml GCCcore/11.2.0 Python/3.9.6
@@ -15,7 +15,7 @@ ml GCC/11.2.0 OpenMPI/4.1.1
 #ml Julia/1.7.1-linux-x86_64  # if Julia is needed
 
 # CHANGE THE PATH BELOW TO YOUR OWN PATH 
-source /proj/nobackup/<your-project-storage>/vpyenv-python-course/bin/activate
+source /proj/nobackup/hpc2n2023-089/<mydir-name>/vpyenv-python-course/bin/activate
 
 mpirun -np 4 python $MYPATH/integration2d_mpi.py
 
