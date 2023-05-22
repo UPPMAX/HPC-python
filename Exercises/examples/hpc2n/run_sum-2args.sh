73a22f7091de404e1980f8db9d1b20241dd1674c
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:57:30 +0200

    Update run_sum-2args.sh

--- Exercises/examples/hpc2n/run_sum-2args.sh
+++ Exercises/examples/hpc2n/run_sum-2args.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=00:05:00 # Asking for 5 minutes
 #SBATCH -n 1 # Asking for 1 core
 
-# Set a path where the example programs are installed, provided you followed the suggestion. 
-# In any case, change the below to your own path to where you placed the example programs
-MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/pythonHPC2N/examples/programs/
+# Set a path where the example programs are installed. 
+# Change the below to your own path to where you placed the example programs
+MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/HPC-python/Exercises/examples/hpc2n/
 
 # Load any modules you need, here for Python 3.9.5
 module purge
