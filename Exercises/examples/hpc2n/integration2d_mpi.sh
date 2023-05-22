371010645723fc022ecf57ee93a9a7c98011456f
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 14:06:33 +0200

    Update integration2d_mpi.sh

--- Exercises/examples/hpc2n/integration2d_mpi.sh
+++ Exercises/examples/hpc2n/integration2d_mpi.sh
@@ -7,7 +7,7 @@
 
 # Set a path where the example programs are installed. 
 # Change the below to your own path to where you placed the example programs
-MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/HPC-python/Exercises/examples/hpc2n/
+MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/HPC-python/Exercises/examples/programs/
 
 ml purge > /dev/null 2>&1
 ml GCCcore/11.2.0 Python/3.9.6
