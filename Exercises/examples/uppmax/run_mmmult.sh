96873d697b55c79797466609fdfb85956dcc6ab5
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:53:03 +0200

    Update run_mmmult.sh

--- Exercises/examples/uppmax/run_mmmult.sh
+++ Exercises/examples/uppmax/run_mmmult.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=00:10:00 # Asking for 10 minutes
 #SBATCH -n 1 # Asking for 1 core
 
-# Set a path where the example programs are installed, provided you followed the suggestion. 
-# In any case, change the below to your own path to where you placed the example programs
-MYPATH=/proj/naiss2023-22-500/<mydir-name>/pythonHPC2N/examples/programs/
+# Set a path where the example programs are installed. 
+# Change the below to your own path to where you placed the example programs.
+MYPATH=/proj/naiss2023-22-500/<mydir-name>/HPC-python/Exercises/examples/uppmax/
 
 # Load any modules you need, here Python 3.9.5.
 module load python/3.9.5
