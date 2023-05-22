d24945694ccd18a545e48d35ce080ef292ad6e69
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:33:39 +0200

    Update example-tf.sh

--- Exercises/examples/hpc2n/example-tf.sh
+++ Exercises/examples/hpc2n/example-tf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A SNIC2022-22-641 # Change to your own after the course
+#SBATCH -A hpc2n2023-089 # Change to your own after the course
 #SBATCH --reservation=hpc-python  # Only valid during the course 
 #SBATCH --time=00:10:00  # Asking for 10 minutes
 # Asking for one K80 card
@@ -15,7 +15,7 @@ module load GCC/10.3.0  OpenMPI/4.1.1 TensorFlow/2.6.0-CUDA-11.3.1
 
 # Activate the virtual environment we installed to
 # CHANGE <path-to-virt-env> to the full path where you installed your virtual environment
-# Example: /proj/snic2022-22-641/nobackup/mrspock/pythonUPPMAX/vpyenv
+# Example: /proj/nobackup/hpc2n2023-089/<mydir-name>/pythonHPC2N/vpyenv
 source <path-to-virt-env>/bin/activate
 
 # Run your Python script
