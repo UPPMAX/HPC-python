2837c4ea35e3ec78084efb928afd8e742df5eb37
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:57:06 +0200

    Update run_serial_virtualenv.sh

--- Exercises/examples/hpc2n/run_serial_virtualenv.sh
+++ Exercises/examples/hpc2n/run_serial_virtualenv.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=00:10:00 # Asking for 10 minutes
 #SBATCH -n 1 # Asking for 1 core
 
-# Set a path where the example programs are installed, provided you followed the suggestion. 
-# In any case, change the below to your own path to where you placed the example programs
-MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/pythonHPC2N/examples/programs/
+# Set a path where the example programs are installed. 
+# Change the below to your own path to where you placed the example programs
+MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/HPC-python/Exercises/examples/hpc2n/
 
 # Load any modules you need, here for Python 3.9.5 and compatible SciPy-bundle
 module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5 SciPy-bundle/2021.05
