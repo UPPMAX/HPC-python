3e406f2a2fd480369e2acbb008607b6445fef332
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2023-05-22 13:56:04 +0200

    Update integration2d_gpu_shared.sh

--- Exercises/examples/hpc2n/integration2d_gpu_shared.sh
+++ Exercises/examples/hpc2n/integration2d_gpu_shared.sh
@@ -14,9 +14,9 @@ ml GCCcore/11.2.0 Python/3.9.6
 ml GCC/11.2.0 OpenMPI/4.1.1
 ml CUDA/11.4.1
 
-# Set a path where the example programs are installed, provided you followed the suggestion. 
-# In any case, change the below to your own path to where you placed the example programs
-MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/pythonHPC2N/examples/programs/
+# Set a path where the example programs are installed. 
+# Change the below to your own path to where you placed the example programs
+MYPATH=/proj/nobackup/hpc2n2023-089/<mydir-name>/HPC-python/Exercises/examples/hpc2n/
 
 # CHANGE TO YOUR OWN PATH AND THE NAME OF YOUR VIRTUAL ENVIRONMENT!
 source /proj/nobackup/hpc2n2023-089/<mydir-name>/<vpyenv-python-course>/bin/activate
