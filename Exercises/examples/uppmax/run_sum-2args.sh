7259453c3723d93df3849740280e02ae95a5fa15
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2023-05-23 16:11:39 +0200

    Updated stuff for UPPMAX exercises

--- Exercises/examples/uppmax/run_sum-2args.sh
+++ Exercises/examples/uppmax/run_sum-2args.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH -A naiss2023-22-500 # Change to your own after the course
 #SBATCH --time=00:05:00 # Asking for 5 minutes
 #SBATCH -n 1 # Asking for 1 core
@@ -8,7 +8,7 @@
 MYPATH=/proj/naiss2023-22-500/<mydir-name>/HPC-python/Exercises/examples/programs/
 
 # Load any modules you need, here for Python 3.9.5
-module load GCC/10.3.0  OpenMPI/4.1.1 Python/3.9.5
+ml python/3.9.5
 
 # Run your Python script
 python $MYPATH/sum-2args.py 2 3
