b8d772bf2896003d36f0a93dc19ffcefbc097f55
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2023-11-29 21:08:00 +0100

    Fixing some of the exercises for UPPMAX

--- Exercises/examples/uppmax/example-tf.sh
+++ Exercises/examples/uppmax/example-tf.sh
@@ -10,12 +10,14 @@
 # Change the below to your own path to where you placed the example programs
 MYPATH=/proj/naiss2023-22-1126/<mydir-name>/HPC-python/Exercises/examples/programs/
 
-module load python_ML_packages python/3.9.5 TensorFlow/2.5.0-fosscuda-2020b scikit-learn/0.22.1
+module load python_ML_packages/3.9.5-gpu python/3.9.5 
+#module load TensorFlow/2.5.0-fosscuda-2020b 
+module load scikit-learn/0.22.1
 
 # If needed, activate the virtual environment we installed to (remove comment out)
 # CHANGE <path-to-virt-env> to the full path where you installed your virtual environment
 # Example: /proj/naiss2023-22-500/<mydir-name>/pythonHPC2N/vpyenv
-#source <path-to-virt-env>/bin/activate
+source <path-to-virt-env>/bin/activate
 
 # Run your Python script
 python $MYPATH/example-tf.py
