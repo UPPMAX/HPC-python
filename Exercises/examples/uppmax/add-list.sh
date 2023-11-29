defa82f9f9067b9d63970ef2b1ca27b6723104e5
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2023-11-29 17:50:16 +0100

    Fixing a project id

--- Exercises/examples/uppmax/add-list.sh
+++ Exercises/examples/uppmax/add-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remember to change this to your own project ID after the course!
-#SBATCH -A naiss2023-22-500 
+#SBATCH -A naiss2023-22-1126
 # We want to run on Snowy
 #SBATCH -M snowy
 # We are asking for 5 minutes
@@ -10,7 +10,7 @@
 
 # Set a path where the example programs are installed. 
 # Change the below to your own path to where you placed the example programs
-MYPATH=/proj/naiss2023-22-500/<mydir-name>/HPC-python/Exercises/examples/programs/
+MYPATH=/proj/naiss2023-22-1126/<mydir-name>/HPC-python/Exercises/examples/programs/
 
 # Remove any loaded modules and load the ones we need
 module purge  > /dev/null 2>&1
@@ -18,7 +18,7 @@ module load python_ML_packages python/3.9.5
 
 
 # Activate the virtual environment we installed to
-source /proj/naiss2023-22-500/<mydir-name>/vpyenv/bin/activate
+source /proj/naiss2023-22-1126/<mydir-name>/vpyenv/bin/activate
 
 # Run your Python script
 python $MYPATH/add-list.py
