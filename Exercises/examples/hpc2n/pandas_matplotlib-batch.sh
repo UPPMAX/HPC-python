949e474af39ded51e567774daf7865b6aab66468
Birgitte Brydsö <bbrydsoe@cs.umu.se>
2024-05-15 15:44:50 +0200

    Re-adding the pandas-matplotlib-batch.sh examples

---
+++ Exercises/examples/hpc2n/pandas_matplotlib-batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Remember to change this to your own project ID!
+#SBATCH -A hpc2n2024-052
+# We are asking for 5 minutes
+#SBATCH --time=00:05:00
+# Asking for one core
+#SBATCH -n 1 
+
+# Change to the directory where the data files and program are located
+# Change the below to your own path to where you placed the example programs
+MYPATH=/proj/nobackup/python-hpc/<mydir-name>/HPC-python/Exercises/examples/programs/
+
+# Remove any loaded modules and load the ones we need
+module purge  > /dev/null 2>&1
+module load GCC/12.3.0 Python/3.11.3 SciPy-bundle/2023.07 matplotlib/3.7.2  
+
+# Run your Python script
+python pandas_matplotlib-batch-kebnekaise.py
