68959335aef28a0c6b763a521e854bf77b64d06d
Birgitte Brydsoe <bbrydsoe@cs.umu.se>
2024-12-04 15:26:41 +0100

    Adding a few exercises for lunarc

---
+++ Exercises/examples/lunarc/example-tf-multiple.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Remember to change this to your own project ID!
+#SBATCH -A lu2024-2-88
+# We are asking for 5 minutes
+#SBATCH --time=00:05:00
+# Asking for one A100 GPU card 
+#SBATCH -p gpua100
+#SBATCH --gres=gpu:1
+
+# Remove any loaded modules and load the ones we need
+module purge  > /dev/null 2>&1
+module load GCC/11.3.0  OpenMPI/4.1.4 TensorFlow/2.11.0-CUDA-11.7.0 scikit-learn/1.1.2  
+
+# Output to file - not needed if your job creates output in a file directly
+# In this example I also copy the output somewhere else and then run another executable (or you could just run the same executable for different parameters).
+
+python <my_tf_program.py> <param1> <param2> > myoutput1 2>&1
+cp myoutput1 mydatadir
+python <my_tf_program.py> <param3> <param4> > myoutput2 2>&1
+cp myoutput2 mydatadir
+python <my_tf_program.py> <param5> <param6> > myoutput3 2>&1
+cp myoutput3 mydatadir
