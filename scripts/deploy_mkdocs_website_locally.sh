2d4e245dba401e894d909739288c1af4b960e9eb
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-02-20 08:41:48 +0100

    Can deploy MkDocs website locally

---
+++ scripts/deploy_mkdocs_website_locally.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Deploys the MkDocs website locally.
+#
+# Usage:
+#
+#   ./scripts/deploy_mkdocs_website_locally.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/deploy_mkdocs_website_locally.sh"
+    echo " "
+    exit 42
+fi
+
+python -m venv hpc_python_venv
+source hpc_python_venv/bin/activate
+pip install -r requirements.txt
+mkdocs serve
