6703570ae53e267ce38defd4f8cd30b67e11c049
Birgitte Brydsö <bbrydsoe@hpc2n.umu.se>
2025-11-28 02:18:11 +0100

    Replacing the gzipped tarball with a bz2zipped tarball for size

--- Exercises/day3/programs/dot.py
+++
@@ -1,9 +0,0 @@
-from time import perf_counter
-import numpy as np
-
-A = np.random.rand(3000,3000)
-starttime = perf_counter()
-B = np.dot(A,A)
-endtime = perf_counter()
-
-print("Time spent: %.2f sec" % (endtime-starttime))
