81b3c311f2c32485215bd45e0e319c1b3c77b8c7
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-11-28 11:14:28 +0100

    Update matplotlib-intro.py

--- Exercises/day3/programs/matplotlib-intro.py
+++ Exercises/day3/programs/matplotlib-intro.py
@@ -1,13 +1,14 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
-matplotlib.use('TkAgg')
+### Comment next 2 lines if NOT running directly from cmd line:
+import matplotlib
+matplotlib.use("TkAgg")
 
 x = np.linspace(-5,5, 50)
 y = 1/(1+np.exp(-x))
 fig, ax = plt.subplots()
 ax.plot(x,y)
 plt.show()
-# comment line above & uncomment line below to save figure; file name up to you
+### comment line above, uncomment line below to save figure (exercise 2)
 #plt.savefig("matplotlib-ex1", format='svg')
+### rename file as desired
