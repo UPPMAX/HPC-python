06f6e19e299742f1816b06adf523cd72f379db7d
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-11-28 16:31:16 +0100

    Create run-seaborn-ex.sh

---
+++ Exercises/day3/uppmax/run-seaborn-ex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Remember to change this to your own project ID!
+#SBATCH -A uppmax2025-2-393
+# We are asking for 5 minutes
+#SBATCH --time=00:05:00
+# Asking for one core (may need more for some pandas exercises)
+#SBATCH -n 1 
+
+# Change to the directory where the data files and program are located
+# Change the below to your own path to where you placed the example programs
+cd /proj/hpc-python-uppmax/<mydir-name>/HPC-python/Exercises/examples/programs/
+
+# Remove any loaded modules and load the ones we need
+module purge  > /dev/null 2>&1
+module load Seaborn/0.13.2-gfbf-2024a #this loads SciPy-bundle, matplotlib, pandas, etc.
+
+# Run your Python script (change as needed)
+python seaborn-ex1.py
