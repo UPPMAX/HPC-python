397a7c0fdb96e63315380f6a7b994fbd0bc19662
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-11-28 13:56:27 +0100

    Update run_serial_virtualenv.sh

--- Exercises/day3/lunarc/run_serial_virtualenv.sh
+++ Exercises/day3/lunarc/run_serial_virtualenv.sh
@@ -7,14 +7,14 @@
 # Change the below to your own path to where you placed the example programs
 MYPATH=/lunarc/nobackup/projects/lu2025-17-52/<your-projecct-storage>/<mydir-name>/HPC-python/Exercises/examples/programs/
 
-# Load any modules you need, here for Python 3.11.3 and compatible SciPy-bundle, matplotlib 
-module load GCC/12.3.0  Python/3.11.3 SciPy-bundle/2023.07 matplotlib/3.7.2
+# Load any modules you need, here for Python 3.11.5 and compatible SciPy-bundle, matplotlib 
+module load GCC/13.2.0 SciPy-bundle/2023.11 matplotlib/3.8.2
 
 # Activate your virtual environment. 
 # CHANGE <path-to-virt-env> to the full path where you installed your 
 # virtual environment. For instance, the vpyenv created in the course 
 # would work with this example 
-# Example: /lunarc/nobackup/projects/lu2025-17-52XXXX-YY-ZZ/<mydir-name>/<myvirtenv>
+# Example: /lunarc/nobackup/projects/lu20YY-XX-ZZ/<mydir-name>/<myvirtenv>
 source <path-to-virt-env>/bin/activate
 
 # Run your Python script - remember to add the name of your script
