3b9816fc615b06bc38de430e31199366c1e9ef08
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-11-28 10:37:41 +0100

    Update matplotlib-intro.py

--- Exercises/day3/programs/matplotlib-intro.py
+++ Exercises/day3/programs/matplotlib-intro.py
@@ -9,3 +9,5 @@ y = 1/(1+np.exp(-x))
 fig, ax = plt.subplots()
 ax.plot(x,y)
 plt.show()
+# comment line above & uncomment line below to save figure; file name up to you
+#plt.savefig("matplotlib-ex1", format='svg')
