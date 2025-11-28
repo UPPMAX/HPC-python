266255edd8007e821abc94ecfab8175af2655d32
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-11-28 13:53:57 +0100

    fix file path run-mpl-pandas-exercises.sh

--- Exercises/day3/lunarc/run-mpl-pandas-exercises.sh
+++ Exercises/day3/lunarc/run-mpl-pandas-exercises.sh
@@ -8,7 +8,7 @@
 
 # Change to the directory where the data files and program are located
 # Change the below to your own path to where you placed the example programs
-cd /lunarc/nobackup/projects/lu2025-17-52/2024-17-44/<mydir-name>/HPC-python/Exercises/examples/programs/
+cd /lunarc/nobackup/projects/lu2025-17-52/<mydir-name>/HPC-python/Exercises/examples/programs/
 
 # Remove any loaded modules and load the ones we need
 module purge  > /dev/null 2>&1
