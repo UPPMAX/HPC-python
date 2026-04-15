b0a88cba5de880be8651b089fa6fb83a3b636fa9
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-04-15 14:26:42 +0200

    Reformat using black

--- docs/day3/matplotlib/matplotlib_exercise.py
+++ docs/day3/matplotlib/matplotlib_exercise.py
@@ -8,8 +8,12 @@ import os
 
 if not os.path.exists("diamonds.csv"):
     # Download the file
-    from urllib.request import urlretrieve    
-    urlretrieve("https://uppmax.github.io/HPC-python/_downloads/2c3f44b9035e3effc9e3b2854f37f1f0/diamonds.csv", "diamonds.csv")
+    from urllib.request import urlretrieve
+
+    urlretrieve(
+        "https://uppmax.github.io/HPC-python/_downloads/2c3f44b9035e3effc9e3b2854f37f1f0/diamonds.csv",
+        "diamonds.csv",
+    )
 
 if not os.path.exists("diamonds.csv"):
     os.sys.exit("Failed to download 'diamonds.csv'")
