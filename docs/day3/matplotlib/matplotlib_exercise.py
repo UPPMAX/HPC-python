76557d972a1f176e65dd97934c3699e92745dd5d
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-04-15 13:52:45 +0200

    Plot the pandas table

--- docs/day3/matplotlib/matplotlib_exercise.py
+++ docs/day3/matplotlib/matplotlib_exercise.py
@@ -1,22 +1,21 @@
+#!/bin/env python
+#
+# Naming this file 'matplotlib.py' will cause problems
+#
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import os
+
 if not os.path.exists("diamonds.csv"):
     # Download the file
     from urllib.request import urlretrieve    
     urlretrieve("https://uppmax.github.io/HPC-python/_downloads/2c3f44b9035e3effc9e3b2854f37f1f0/diamonds.csv", "diamonds.csv")
 
 if not os.path.exists("diamonds.csv"):
-    sys.exit("Failed to download 'diamonds.csv'")
+    os.sys.exit("Failed to download 'diamonds.csv'")
 
 table = pd.read_csv("diamonds.csv")
-df = pd.DataFrame(table)
 
 
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
-
-plt.show()    
-plt.figure().savefig('matplotlib.png')
+plt.scatter(table["carat"], table["price"])
+plt.savefig("matplotlib_exercise.png")
